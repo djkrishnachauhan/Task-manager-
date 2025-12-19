8800440fb4d7e0043b68650d42c69565daf522ce
KRISHH CHAUHAN <95200141+djkrishnachauhan@users.noreply.github.com>
2025-12-19 12:01:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
-const express = require("express");
-const cors = require("cors");
 require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-require("./db");
-
+const authRoutes = require("./routes/auth");
 const taskRoutes = require("./routes/tasks");
+const userRoutes = require("./routes/users");
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB Connected"))
+    .catch(err => console.error(err));
+
+app.use("/auth", authRoutes);
 app.use("/tasks", taskRoutes);
+app.use("/users", userRoutes);
 
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-    console.log(`🚀 Task API running at http://localhost:${PORT}`);
-});
+app.listen(PORT, () => console.log("Server running on", PORT));
